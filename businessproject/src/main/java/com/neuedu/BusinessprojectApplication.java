038433300b4c3f497dc33ee0d4d20c0e6b1473de
jiachunl <52821907+jiachunl@users.noreply.github.com>
2020-03-07 15:26:43 +0800

    12

---
+++ businessproject/src/main/java/com/neuedu/BusinessprojectApplication.java
@@ -0,0 +1,13 @@
+package com.neuedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessprojectApplication.class, args);
+    }
+
+}
